5fa0341132abbe2e62049d21587612d7aab916cb
jjhonny <ojhonny1@hotmail.com>
2024-04-25 20:53:38 -0300

    feat: generates static page for number of posts and comments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import Image from "next/image";
 
 import heroImg from "../../public/assets/hero.png";
+import { db } from "../services/firebaseConnection"
+import { collection, getDocs } from "firebase/firestore";
 
-export default function Home() {
+
+interface HomeProps {
+  posts: number;
+  comments: number;
+}
+
+export default function Home({ posts, comments }: HomeProps) {
   return (
     <>
       <div className={styles.container}>
@@ -27,14 +36,31 @@ export default function Home() {
           </h1>
           <div className={styles.infoContent}>
             <section className={styles.box}>
-              <span>+12 posts</span>
+              <span>+{posts} posts</span>
             </section>
             <section className={styles.box}>
-              <span>+90 comentários</span>
+              <span>+{comments} comentários</span>
             </section>
           </div>
         </main>
       </div>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const commentRef = collection(db, "comments");
+  const postRef = collection(db, "tarefas");
+
+  const commentSnapshot = await getDocs(commentRef);
+  const postSnapshot = await getDocs(postRef);
+
+  return {
+    props: {
+      posts: postSnapshot.size || 0,
+      comments: commentSnapshot.size || 0,
+    },
+    revalidate: 60 // revalida a cada 60 segundos
+  }
 }
